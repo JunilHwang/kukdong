56ac877c066c274001e08fe51017247fe32a1420
junilHwang <tjsdlf4261@naver.com>
2018-02-02 20:20:33 +0900

    제품 작업중

---
+++ js/data.js
@@ -0,0 +1,256 @@
+var product:{
+	"MIL-DTL-5015A":{
+		characteristic:[
+			"Threaded coupling",
+			"Soldering (soldering) contact structure",
+			"Excellent durability",
+			"Various product group and contact array",
+		],
+		app:[
+			"",
+		]
+	},
+	"MIL-DTL-5015B":{
+		characteristic:[
+			"Threaded coupling",
+			"Soldering (soldering) contact structure",
+			"Excellent durability",
+			"Various product group and contact array",
+		],
+		app:[
+			"",
+		]
+	},
+	"KDB series (VG95234)":{
+		characteristic:[
+			"Bayonet connection method",
+			"Clipping contact structure",
+			"Strong against vibration / impact",
+			"Rapid attachment and detachment (excellent fastening property)",
+		],
+		app:[
+			"",
+		]
+	},
+	"MIL-DTL-22992 series":{
+		characteristic:[
+			"Large capacity power transmission",
+			"Double-threaded type",
+			"Stable / Easy Combination",
+		],
+		app:[
+			"Various generators (5/10/60 / 100KW)",
+		]
+	},
+	"MIL-DTL-26482 Series I":{
+		characteristic:[
+			"Bayonet connection method",
+			"Soldering (soldering) contact structure",
+			"Stable / accurate coupling (5KEYWAY)",
+			"Small, excellent environmental resistance",
+		],
+		app:[
+			"Military equipment, missile accelerator",
+			"LED street light, LED flood light",
+			"Aerospace Center (Naro Space Center)",
+		]
+	},
+	"MIL-DTL-26482 Series II":{
+		characteristic:[
+			"Bayonet connection method",
+			"Clipping contact structure",
+			"Stable / accurate coupling (5KEYWAY)",
+			"Excellent EMI shielding / signal transmission capability",
+		],
+		app:[
+			"New CBC Reconnaissance Vehicle",
+			"Harbor patrol boat",
+			"TICN - air conditioner, etc.",
+		]
+	},
+	"MIL-DTL-55116(FOR AUDIO)":{
+		characteristic:[
+			"Voice signal transmission",
+			"KEY-HOME coupling method",
+			"Waterproof design",
+			"Spring-loaded contacts (minimize contact resistance)",
+		],
+		app:[
+			"",
+		]
+	},
+	"MIL-DTL-55181(MW series)":{
+		characteristic:[
+			"Combine screw jack (tightening screw)",
+			"Excellence in waterproof ability",
+		],
+		app:[
+			"",
+		]
+	},
+	"ADAPTER (KAT/KH Series)":{
+		characteristic:[
+			"Classified by TR / BAND type",
+			"Electromagnetic shielding (Essential items)",
+			"Can be manufactured with 0 ° / 45 ° / 90 °",
+		],
+		app:[
+			"K21 Infantry Fighting Vehicle - Mounting Components for EMI Shielding",
+			"Components: KDB connector + KAT / KH adapter + EMI shielding cable",
+		]
+	},
+	"SOLENOID VALVE":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"-",
+		]
+	},
+	"HIGH CURRENT CONNECTOR":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"-",
+		]
+	},
+	"POWER START CONNECTO":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"-",
+		]
+	},
+	"Cable Assembly for cooling Fan Controller for Armored Vehicle":{
+		characteristic:[
+			"Temperature: -55 ° C to 135 ° C",
+			"Waterproof Type",
+			"EMI shielding design, etc.",
+		],
+		app:[
+			"Aerospace Center, (participating in Naro Space Center construction project)",
+			"Unmanned aircraft, guided weapons, military detection radar, etc.",
+		]
+	},
+	"Cable Assembly for Military Generator":{
+		characteristic:[
+			"Temperature: -55 ° C to 135 ° C",
+			"5KW, 16KW transmission",
+			"EMI shielding design, waterproof type, etc.",
+		],
+		app:[
+			"TICN-Power System, Small Tactical Vehicle",
+			"Generators for K311 and K511 (5KW, 16KW), etc.",
+		]
+	},
+	"Cable Assembly for Equipment Military Communications by Satellite":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"-",
+		]
+	},
+	"Cable Assembly for Military Protection Equipment":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"-",
+		]
+	},
+	"Cable Assembly for Military Sound-detecting Equipment":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"Acoustic equipment (for ground detection sensors)",
+		]
+	},
+	"Cable Assembly for Welding Machine at shipbuilding Yards":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"Welding machine for shipyard",
+		]
+	},
+	"AC Power Outlet at Outdoor Shipbuilding Yards":{
+		characteristic:[
+			"-",
+		],
+		app:[
+			"For operation inside shipyard hull power supply",
+		]
+	},
+	"Cable Assembly for Military Communication System (CX-518K)":{
+		characteristic:[
+			"Temperature: -55 ° C to 135 ° C",
+			"Waterproof type and others",
+		],
+		app:[
+			"Automotive and various industrial equipment",
+		]
+	},
+	"Cable Assembly for Military Communication System (CX-513K)":{
+		characteristic:[
+			"Temperature: -55 ° C to 135 ° C",
+			"Waterproof type and others",
+		],
+		app:[
+			"Automotive and various industrial equipment",
+		]
+	},
+	"Cooling Fan Controller of Armored Vehicle Engine":{
+		characteristic:[
+			"Temperature: -35 ° C to 50 ° C",
+			"Frequency range: 2 to 29.9999 MHz",
+		],
+		app:[
+			"AM radio (PRC-950K)",
+		]
+	},
+	"Battery Box for Aircraft":{
+		characteristic:[
+			"Temperature: -35 ° C to 50 ° C",
+			"Frequency range: 2 to 29.9999 MHz",
+		],
+		app:[
+			"AM radio (PRC-950K)",
+		]
+	},
+	"Radio Handset (H-350K)":{
+		characteristic:[
+			"",
+		],
+		app:[
+			"",
+		]
+	},
+	"Dipole Antenna for AM Radio (AS-814K)":{
+		characteristic:[
+			"",
+		],
+		app:[
+			"",
+		]
+	},
+	"Limit switch for Armored Vehicle":{
+		characteristic:[
+			"",
+		],
+		app:[
+			"",
+		]
+	},
+	"XENON Portable Searchlight (RAYON & RAYON45)":{
+		characteristic:[
+			"",
+		],
+		app:[
+			"",
+		]
+	},
+}
